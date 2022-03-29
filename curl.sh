9faeb5f8b85a7de874c5622460617d63a7099ec6
Zahrajsi.net <nextikczcz@gmail.com>
2022-03-29 14:55:25 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 ## Config ##
-CDN = ""
-URL = ""
+Your_CDN = ""
+Your_URL = ""
+#####################
 
-## Colors ##"
-GREEN = "\e[32m"
-RED   = "\e[31m"
+## Colors ##
+G = "\e[32m"
+R = "\e[31m"
+Y = "\e[33m"
+E = "\e[0m" ## Make Default Color
+#####################
+
+## Read_Input ##
+read -p 'YourFolder to Upload: ' folder
+read -p 'Your Link to Provide: ' link
+read -p 'Custom Format?: ' format
+###############################################################
 
 ## Application ##
-date=$(date '+%Y-%m-%d %H:%M:%S')
+_date_      = $(date '+%Y-%m-%d %H:%M:%S')
+_random_    = $(cat /dev/urandom | env LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+_http_code_ = $(curl -s -o "l.log" -w "%{http_code}" "$link")
+#########################################################################################################
+
+## Main ##
+
